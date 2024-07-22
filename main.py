11703d71628530ec3259cca8c47c6f3060e543e9
zhansul19 <zhansultanjan@gmail.com>
2024-07-22 10:43:35 +0500

    postgres

--- main.py
+++ main.py
@@ -75,50 +75,52 @@ async def read_photo(iin: str):
     conn = connect_to_db()
     if conn:
         cursor = conn.cursor()
-        cursor.execute("""select 
-             fp."IIN", 
-             fp."FIRSTNAME",
-             fp."SURNAME",
-              fp."SECONDNAME",
+        cursor.execute("""
+        select 
+         fp."IIN", 
+         fp."FIRSTNAME",
+         fp."SURNAME",
+         fp."SECONDNAME",
+         case 
+            when fp."SEX_ID"='1' then 'Мужчина'
+            when fp."SEX_ID"='2' then 'Женщина'
+         end as SEX,
+         fp."BIRTH_DATE",
+         
+         dc."RU_NAME" as "BIRTH_COUNTRY_RU",
+         dc."KZ_NAME"as "BIRTH_COUNTRY_KZ",
+          
+         
+         dd."RU_NAME" as "BIRTH_DISTRICT_NAME_RU",
+         dd."KZ_NAME" as "BIRTH_DISTRICT_NAME_KZ",
+         dr."RU_NAME" as "BIRTH_REGION_NAME_RU",
+         dr."KZ_NAME" as "BIRTH_REGION_NAME_KZ",
+        
+        fp."BIRTH_CITY",
+        dc2."RU_NAME" as "CITIZENSHIP",
+         dn."RU_NAME" as "NATIONALITY_RU",
+         dn."KZ_NAME" as "NATIONALITY_KZ",
+         
+         ddtn."RU_NAME" as "DOCUMENT_TYPE",
+         fd."DOCUMENT_NUMBER",
+         fd."DOCUMENT_BEGIN_DATE",
+         fd."DOCUMENT_END_DATE",
+         ddo."RU_NAME" as "ISSUE_ORGANIZATION"
+        from imp_kfm_fl.fl_person fp 
+        left outer join imp_kfm_fl.fl_document fd  on fp."ID"  = fd."PERSON_ID" 
+        left join "dictionary".d_country dc on fp."BIRTH_COUNTRY_ID" = cast(dc."ID" as text )
+        left join "dictionary".d_country dc2 on fp."CITIZENSHIP_ID" = cast(dc2."ID" as text )
 
-             case 
-                when fp."SEX_ID"='1' then 'Мужчина'
-                when fp."SEX_ID"='2' then 'Женщина'
-             end as SEX,
-             fp."BIRTH_DATE",
-             
-             dc."RU_NAME" as "BIRTH_COUNTRY_RU",
-             dc."KZ_NAME"as "BIRTH_COUNTRY_KZ",
-             
-             dd."RU_NAME" as "BIRTH_DISTRICT_NAME_RU",
-             dd."KZ_NAME" as "BIRTH_DISTRICT_NAME_KZ",
-             dr."RU_NAME" as "BIRTH_REGION_NAME_RU",
-             dr."KZ_NAME" as "BIRTH_REGION_NAME_KZ",
-            
-             fp."BIRTH_CITY",
-             fp."CITIZENSHIP_ID",
-             
-             dn."RU_NAME" as "NATIONALITY_RU",
-             dn."KZ_NAME" as "NATIONALITY_KZ",
-             
-             ddtn."RU_NAME" as "DOCUMENT_TYPE",
-             fd."DOCUMENT_NUMBER",
-             fd."DOCUMENT_BEGIN_DATE",
-             fd."DOCUMENT_END_DATE",
-             ddo."RU_NAME" as "ISSUE_ORGANIZATION"
-            from imp_kfm_fl.fl_person fp 
-            left outer join imp_kfm_fl.fl_document fd  on fp."ID"  = fd."PERSON_ID" 
-            inner join "dictionary".d_country dc on fp."BIRTH_COUNTRY_ID" = cast(dc."ID" as text )
-            inner join "dictionary".d_nationality_new dn on fp."NATIONALTY_ID" =cast(dn."ID"  as text )
-            inner join "dictionary".d_districts dd on fp."BIRTH_DISTRICTS_ID"  =cast(dd."ID"  as text )
-            inner join "dictionary".d_region dr on fp."BIRTH_REGION_ID"  =cast(dr."ID"  as text )
-            inner join "dictionary".d_document_type_new ddtn on fd."DOCUMENT_TYPE_ID" =cast(ddtn."ID"  as text )
-            inner join "dictionary".d_doc_organization ddo on fd."ISSUE_ORGANIZATION_ID" =cast(ddo."ID"  as text )
-            
-            
-            where fp."IIN" like %s
-            AND ddtn."ID" = 2 
-            order by fd."DOCUMENT_BEGIN_DATE" desc
+        left join "dictionary".d_nationality_new dn on fp."NATIONALTY_ID" =cast(dn."ID"  as text )
+        left join "dictionary".d_districts dd on fp."BIRTH_DISTRICTS_ID"  =cast(dd."ID"  as text )
+        left join "dictionary".d_region dr on fp."BIRTH_REGION_ID"  =cast(dr."ID"  as text )
+        left join "dictionary".d_document_type_new ddtn on fd."DOCUMENT_TYPE_ID" =cast(ddtn."ID"  as text )
+        left join "dictionary".d_doc_organization ddo on fd."ISSUE_ORGANIZATION_ID" =cast(ddo."ID"  as text )
+        
+        
+        where fp."IIN" like %s
+        AND ddtn."ID" = 2 
+        order by fd."DOCUMENT_BEGIN_DATE" desc
 """, (iin,))
         row = cursor.fetchone()
         conn.close()
@@ -159,9 +161,29 @@ async def read_photo(iin: str):
         result = client.query(query2, parameters={'iin': iin})
         if row:
             data = {
-                "postgres":row,
+                "IIN": row[0],
+                "FIRSTNAME": row[1],
+                "SURNAME": row[2],
+                "SECONDNAME": row[3],
+                "SEX": row[4],
+                "BIRTH_DATE": row[5],
+                "BIRTH_COUNTRY_RU": row[6],
+                "BIRTH_COUNTRY_KZ": row[7],
+                "BIRTH_DISTRICT_NAME_RU": row[8],
+                "BIRTH_DISTRICT_NAME_KZ": row[9],
+                "BIRTH_REGION_NAME_RU": row[10],
+                "BIRTH_REGION_NAME_KZ": row[11],
+                 "BIRTH_CITY": row[12],
+                 "CITIZENSHIP": row[13],
+                "NATIONALITY_RU": row[14],
+                "NATIONALITY_KZ": row[15],
+                "DOCUMENT_TYPE": row[16],
+                "DOCUMENT_NUMBER": row[17],
+                "DOCUMENT_BEGIN_DATE": row[18],
+                "DOCUMENT_END_DATE": row[19],
+                "ISSUE_ORGANIZATION": row[20],
 
-                "data": result.named_results()
+                 "data": result.named_results()
             }
 
             return data
