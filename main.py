c9decd9a9c6a1fed266add40bd3cc5f849582e04
zhansul19 <zhansultanjan@gmail.com>
2024-09-23 11:17:47 +0500

    postgres password change

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ async def read_photo(iin: str):
         raise HTTPException(status_code=500, detail="Failed to connect to the database")
 
 
-@app.get("/get_data/{iin}")
+@app.get("/get_data2/{iin}")
 async def read_data(iin: str):
     conn = connect_to_db()
     if conn:
@@ -227,67 +227,145 @@ async def read_data(iin: str):
         raise HTTPException(status_code=500, detail="Failed to connect to the database")
 
 
-@app.get("/get_data2/{iin}")
-async def read_photo(iin: str):
-    query2 = """SELECT 
-            s.iin AS IIN,
+@app.get("/get_data/{iin}")
+async def read_data(iin: str):
+    try:
+        client = clickhouse_connect.get_client(
+            user='nifitest',
+            password='nifitest',
+            host='192.168.122.45',
+            port=8123
+        )
+
+        query1 = """
+        SELECT 
+            dd.IIN IIN,
+            dd.FIRSTNAME FIRSTNAME,
+            dd.SURNAME SURNAME,
+            dd.SECONDNAME SECONDNAME,
+            CASE 
+                WHEN dd.SEX_ID = '1' THEN 'Мужчина'
+                WHEN dd.SEX_ID = '2' THEN 'Женщина'
+            END AS SEX,
+            dd.BIRTH_DATE BIRTH_DATE,
+            dc.RU_NAME AS BIRTH_COUNTRY_RU,
+            dc.KZ_NAME AS BIRTH_COUNTRY_KZ,
+            dd2.RU_NAME AS BIRTH_DISTRICT_NAME_RU,
+            dd2.KZ_NAME AS BIRTH_DISTRICT_NAME_KZ,
+            dru.RU_NAME AS BIRTH_REGION_NAME_RU,
+            dru.KZ_NAME AS BIRTH_REGION_NAME_KZ,
+            dd.BIRTH_CITY BIRTH_CITY,
+            dc2.RU_NAME AS CITIZENSHIP,
+            n.RU_NAME AS NATIONALITY_RU,
+            n.KZ_NAME AS NATIONALITY_KZ,
+            dd.DOCUMENT_TYPE_ID DOCUMENT_TYPE,
+            dd.DOCUMENT_NUMBER DOCUMENT_NUMBER,
+            dd.DOCUMENT_BEGIN_DATE DOCUMENT_BEGIN_DATE,
+            dd.DOCUMENT_END_DATE DOCUMENT_END_DATE,
+            dd.ISSUE_ORGANIZATION_ID ISSUE_ORGANIZATION
+        FROM db_fl_ul_dpar.damp_document dd 
+        INNER JOIN db_fl_ul_dpar.DIC_COUNTRY dc ON dd.BIRTH_COUNTRY_ID = toString(dc.ID)
+        INNER JOIN db_fl_ul_dpar.DIC_DISTRICTS dd2 ON dd.BIRTH_DISTRICTS_ID = toString(dd2.ID)
+        INNER JOIN db_fl_ul_dpar.DIC_REGION_uniq dru ON dd.BIRTH_REGION_ID = toString(dru.ID)
+        INNER JOIN db_fl_ul_dpar.DIC_COUNTRY dc2 ON dd.CITIZENSHIP_ID = toString(dc2.ID)
+        INNER JOIN db_fl_ul_dpar.nationality n ON dd.NATIONALTY_ID = toString(n.ID) AND dd.SEX_ID = n.SEX
+        WHERE dd.IIN = %(iin)s 
+        AND dd.DOCUMENT_TYPE_ID = 'УДОСТОВЕРЕНИЕ РК'
+        ORDER BY dd.DOCUMENT_BEGIN_DATE DESC
+        LIMIT 1
+        """
+
+        query2 = """
+        SELECT 
+            nb.iin_ AS IIN,
             s.study_info AS STUDY,
             s2.school_info AS SCHOOL,
             ra.`Адрес на русском` AS ADDRESS,
             nb.phonenumber_ AS PHONE_NUMBER
-        FROM (
-            SELECT 
-                iin,
-                groupArray(tuple(study_code AS study_code, study_name AS study_name, start_date AS start_date, end_date AS end_date)) AS study_info
-            FROM 
-                db_fl_ul_dpar.study
-            GROUP BY 
-                iin
-        ) AS s
+        FROM db_fl_ul_dpar.numb AS nb 
         LEFT JOIN (
             SELECT 
                 iin,
-                groupArray(tuple(school_code AS school_code, school_name AS school_name, start_date AS start_date, end_date AS end_date)) AS school_info
-            FROM 
-                db_fl_ul_dpar.school
-            GROUP BY 
-                iin
+                groupArray(tuple(study_code, study_name, start_date, end_date)) AS study_info
+            FROM db_fl_ul_dpar.study
+            GROUP BY iin
+        ) AS s ON s.iin = nb.iin_
+        LEFT JOIN (
+            SELECT 
+                iin,
+                groupArray(tuple(school_code, school_name, start_date, end_date)) AS school_info
+            FROM db_fl_ul_dpar.school
+            GROUP BY iin
         ) AS s2 ON s.iin = s2.iin
-        LEFT JOIN 
-            db_fl_ul_dpar.reg_address AS ra ON s.iin = ra.`ИИН/БИН`
-        LEFT JOIN
-            db_fl_ul_dpar.numb AS nb ON s.iin = nb.iin_
-        WHERE 
-            s.iin = %(iin)s 
-                   """
-    result = client.query(query2, parameters={'iin': iin})
+        LEFT JOIN db_fl_ul_dpar.reg_address AS ra ON nb.iin_ = ra.`ИИН/БИН`
+        WHERE nb.iin_ = %(iin)s 
+        SETTINGS join_algorithm = 'parallel_hash'
+        """
 
-    data = result.named_results()
+        result1 = client.query(query1, parameters={'iin': iin})
+        row = list(result1.named_results())  # Convert generator to list
 
-    # Process the results
-    for row in data:
-        study_info = [
-            {
-                'study_code': study[0],
-                'study_name': study[1],
-                'start_date': study[2],
-                'end_date': study[3]
+        result2 = client.query(query2, parameters={'iin': iin})
+        data2 = list(result2.named_results())  # Convert generator to list
+
+        if row and data2 :
+            processed_data = []
+            for row1 in data2:
+                study_info = [
+                    {
+                        'study_bin': study[0],
+                        'study_name': study[1],
+                        'start_date': study[2],
+                        'end_date': study[3]
+                    }
+                    for study in row1['STUDY']
+                ]
+                school_info = [
+                    {
+                        'school_bin': school[0],
+                        'school_name': school[1],
+                        'start_date': school[2],
+                        'end_date': school[3]
+                    }
+                    for school in row1['SCHOOL']
+                ]
+                row1['STUDY'] = study_info
+                row1['SCHOOL'] = school_info
+                processed_data.append(row1)
+
+            data = {
+                "IIN": row[0]['IIN'],
+                "FIRSTNAME": row[0]['FIRSTNAME'],
+                "SURNAME": row[0]['SURNAME'],
+                "SECONDNAME": row[0]['SECONDNAME'],
+                "SEX": row[0]['SEX'],
+                "BIRTH_DATE": row[0]['BIRTH_DATE'],
+                "BIRTH_COUNTRY_RU": row[0]['BIRTH_COUNTRY_RU'],
+                "BIRTH_COUNTRY_KZ": row[0]['BIRTH_COUNTRY_KZ'],
+                "BIRTH_DISTRICT_NAME_RU": row[0]['BIRTH_DISTRICT_NAME_RU'],
+                "BIRTH_DISTRICT_NAME_KZ": row[0]['BIRTH_DISTRICT_NAME_KZ'],
+                "BIRTH_REGION_NAME_RU": row[0]['BIRTH_REGION_NAME_RU'],
+                "BIRTH_REGION_NAME_KZ": row[0]['BIRTH_REGION_NAME_KZ'],
+                "BIRTH_CITY": row[0]['BIRTH_CITY'],
+                "CITIZENSHIP": row[0]['CITIZENSHIP'],
+                "NATIONALITY_RU": row[0]['NATIONALITY_RU'],
+                "NATIONALITY_KZ": row[0]['NATIONALITY_KZ'],
+                "DOCUMENT_TYPE": row[0]['DOCUMENT_TYPE'],
+                "DOCUMENT_NUMBER": row[0]['DOCUMENT_NUMBER'],
+                "DOCUMENT_BEGIN_DATE": row[0]['DOCUMENT_BEGIN_DATE'],
+                "DOCUMENT_END_DATE": row[0]['DOCUMENT_END_DATE'],
+                "ISSUE_ORGANIZATION": row[0]['ISSUE_ORGANIZATION'],
+                "data": processed_data
             }
-            for study in row['STUDY']
-        ]
-        school_info = [
-            {
-                'school_code': school[0],
-                'school_name': school[1],
-                'start_date': school[2],
-                'end_date': school[3]
-            }
-            for school in row['SCHOOL']
-        ]
-        row['STUDY'] = study_info
-        row['SCHOOL'] = school_info
 
-    return row
+            return data
+        else:
+            raise HTTPException(status_code=404, detail="No data found for the provided IIN.")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 @app.get("/get_relatives/{iin}")
 async def get_relatives(iin: str):
     if len(iin) != 12:
